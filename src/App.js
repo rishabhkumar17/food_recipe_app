6750411732b4524d46a4d5deb6332d0f13499543
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-09-26 05:09:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { YOUR_APP_ID, YOUR_APP_KEY } from './key'
+import Axios from 'axios'
 
 function App() {
+  let url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&&health=alcohol-free`
+
+  async function getRecipes() {
+    let result = await Axios.get(url)
+    console.log(result.data)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 onClick={getRecipes}>Hello Rishabh</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
