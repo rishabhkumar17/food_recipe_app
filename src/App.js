dd97ffb0aad2138eca11b995d3dcf831faf71c4b
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-09-26 05:40:06 +0530

    input and search button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css'
 import { YOUR_APP_ID, YOUR_APP_KEY } from './key'
 import Axios from 'axios'
+import { useState } from 'react'
 
 function App() {
+  const [query, setQuery] = useState('')
+
   let url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&&health=alcohol-free`
 
   async function getRecipes() {
@@ -10,8 +13,18 @@ function App() {
     console.log(result.data)
   }
   return (
-    <div className="App">
-      <h1 onClick={getRecipes}>Hello Rishabh</h1>
+    <div className="app">
+      <h1>Food Recipe</h1>
+      <form className="app__searchform">
+        <input
+          type="text"
+          className="app__input"
+          placeholder="Enter ingredient"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <input className="app__submit" type="submit" placeholder="Search" />
+      </form>
     </div>
   )
 }
