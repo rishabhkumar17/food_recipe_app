c8c146334e98a5919eb2edafc1e6f8739cc46b8e
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-09-27 05:29:01 +0530

    healthLabels

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import RecipeTile from './RecipeTile'
 function App() {
   const [query, setQuery] = useState('')
   const [recipes, setRecipes] = useState([])
+  const [healthLabel, setHealthLabel] = useState('vegan')
 
-  let url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&&health=alcohol-free`
+  let url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=${healthLabel}`
 
   async function getRecipes() {
     let result = await Axios.get(url)
@@ -33,6 +34,21 @@ function App() {
           onChange={(e) => setQuery(e.target.value)}
         />
         <input className="app__submit" type="submit" placeholder="Search" />
+        <select className="app__healthLabels">
+          <option onClick={() => setHealthLabel('vegan')}>vegan</option>
+          <option onClick={() => setHealthLabel('dairy-free')}>
+            dairy-free
+          </option>
+          <option onClick={() => setHealthLabel('gluten-free')}>
+            gluten-free
+          </option>
+          <option onClick={() => setHealthLabel('wheat-free')}>
+            wheat-free
+          </option>
+          <option onClick={() => setHealthLabel('fat-free')}>fat-free</option>
+          <option onClick={() => setHealthLabel('low-sugar')}>low-sugar</option>
+          <option onClick={() => setHealthLabel('soy-free')}>soy-free</option>
+        </select>
       </form>
       <div className="app__recipes">
         {recipes.map((recipe) => {
