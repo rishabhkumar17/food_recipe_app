7ff681bd05b9f5ec11b424aa0e5f2c636b913e42
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-09-27 04:27:58 +0530

    Recipe tile name and image

--- src/RecipeTile.js
+++ src/RecipeTile.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
-export default function RecipeTile() {
-  return <div></div>
+export default function RecipeTile({ recipe }) {
+  return (
+    <div className="recipeTile">
+      <img className="recipeTile__img" src={recipe['recipe']['image']} />
+      <p className="recipeTile__name">{recipe['recipe']['label']}</p>
+    </div>
+  )
 }
